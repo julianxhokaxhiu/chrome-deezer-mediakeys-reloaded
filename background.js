8af31653b717df633fec74ffdc7303a3fe52896d
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2016-03-30 11:38:44 +0200

    Fix https://bugs.chromium.org/p/chromium/issues/detail?id=592056
    Bump version for update

--- background.js
+++ background.js
@@ -2,8 +2,12 @@ chrome.commands.onCommand.addListener(function(command){
 	chrome.tabs.query({
         url: '*://*.deezer.com/*'
     }, function(tabs){
-        chrome.tabs.sendMessage( tabs[0].id, {
-            action: command
-        });
+        for ( var i in tabs ) {
+          var tab = tabs[i];
+
+          chrome.tabs.sendMessage( tab.id, {
+              action: command
+          });
+        }
     })
 });
