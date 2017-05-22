602507e5bf4e58e84427769781a451ff7b1c2480
Nihad Abbasov <nihad@42na.in>
2017-05-23 00:59:46 +0400

    show notification with song name when a new song starts to play

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
+window.addEventListener('load', function() {
+    var currentSong = document.querySelector('.player-track-title').textContent,
+        song, artist;
+
+    function notify() {
+      song = document.querySelector('.player-track-title').textContent;
+
+      if (song !== currentSong) {
+        artist = document.querySelector('.player-track-artist').textContent;
+        artist = artist.replace('by ', '');
+
+        chrome.runtime.sendMessage({artist: artist, song: song});
+        currentSong = song;
+      }
+    }
+
+    setInterval(function() { notify(); }, 1000);
+});
+
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse){
     var click_event = new MouseEvent('click', {
         'view': window,
         'bubbles': true,
         'cancelable': true
     });
+
     switch(request.action){
         case 'NEXT-MK':
             document.getElementsByClassName('control-next')[0].dispatchEvent(click_event);
@@ -16,10 +36,7 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse){
                 document.getElementsByClassName('control-pause')[0].dispatchEvent(click_event);
             break;
         case 'PLAY-PAUSE-MK':
-            if ( document.getElementsByClassName('control-play').length > 0 )
-                document.getElementsByClassName('control-play')[0].dispatchEvent(click_event);
-            else
-                document.getElementsByClassName('control-pause')[0].dispatchEvent(click_event);
+            document.getElementsByClassName('control-play')[0].dispatchEvent(click_event);
             break;
     }
 });
