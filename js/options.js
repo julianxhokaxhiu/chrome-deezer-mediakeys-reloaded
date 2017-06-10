ac7f454eee5a5345685a5e1b774c702738b26438
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-06-10 16:36:52 +0200

    Introduce a status text
    Inform the user when the options are saved.

--- js/options.js
+++ js/options.js
@@ -12,5 +12,12 @@ function load() {
 function save() {
     chrome.storage.sync.set({
         "notifications": document.getElementById("notifications").checked
+    }, function() {
+      // Update status to let user know options were saved.
+      var status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 750);
     });
 }
