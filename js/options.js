85a056c03c38311fef5aa4921af85a7f336c4080
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-06-10 16:42:14 +0200

    Enhance the behavior when options are saved
    If the sync was successful close the option window, otherwise inform the user about the error and do nothing.

--- js/options.js
+++ js/options.js
@@ -13,11 +13,14 @@ function save() {
     chrome.storage.sync.set({
         "notifications": document.getElementById("notifications").checked
     }, function() {
-      // Update status to let user know options were saved.
       var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
+      if ( chrome.runtime.lastError ) {
+        status.textContent = chrome.runtime.lastError;
+      } else { // Update status to let user know options were saved.
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+          window.close()
+        }, 1000);
+      }
     });
 }
